df63b4caa3196ea34652fe3b34baa898c3e33e29
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-07-19 00:52:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(superbowls){
+  let win = superbowls.find(superbowl => superbowl.result === "W")
+  return win ? win.year : undefined;
+}
